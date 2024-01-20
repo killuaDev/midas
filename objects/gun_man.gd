b84e77fe4f9fcaaf44f6caef98ec8113bcb480c7
killuaDev <louis.l.cullen@gmail.com>
2024-01-20 16:15:05 +0000

    cleanup and created a base for a GunMan class

--- objects/gun_man.gd
+++ objects/gun_man.gd
@@ -1,6 +1,6 @@
-extends Node
+extends StaticBody3D
 
-@export var items: Array[Item] = []
+@export var held_item: Pistol
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +10,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+#TODO: state machine
+# - if not in range of player it'll go towards the player
+# - if in range it'll shoot
